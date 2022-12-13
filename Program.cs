2760f80f5995bb18600dc763fb5290e5f56f0601
Svenlaa <sven.lammertink@hotmail.com>
2022-12-13 10:23:09 +0100

    Change Route

--- Program.cs
+++ Program.cs
@@ -3,16 +3,14 @@ using System.Net;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string mediaId = "";
 const string mediaFormat = "jpg";
 WebClient webClient = new WebClient();
 
-app.MapGet("/do", () =>
+app.MapGet("/downloadFile/{mediaId}", (string mediaId) =>
 {
     var dateTime = DateTime.Now;
     webClient.DownloadFileAsync(new Uri($"https://pbs.twimg.com/media/{mediaId}?format={mediaFormat}"), $"D:\\temp\\{dateTime:hhmmss}.png");
-    
-    return "最高";
+    return "Downloaded";
 });
 
 app.Run();
